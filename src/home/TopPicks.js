6d753b74cf1ce5a7f404a7810d12c9a8c5771bcd
Paxton Bigler <paxton.bigler@gmail.com>
2016-11-01 20:27:30 -0600

    added frame for mobile top picks carousel

--- src/home/TopPicks.js
+++ src/home/TopPicks.js
@@ -1,10 +1,36 @@
 import React from 'react';
+var Slider = require('react-slick');
+require ('./picks.component.scss');
+require ('./home.variables.scss');
 
 export default React.createClass({
   render(){
+    var settings = {
+       dots: true,
+       infinite: true,
+       speed: 500,
+       slidesToShow: 1,
+       slidesToScroll: 1
+     };
     return(
-      <div>
-        Top Picks
+      <div className="top-picks-container">
+        <div className="top-picks-title">
+          our top picks
+        </div>
+        <div className="large-pad"></div>
+        <Slider {...settings} className='slider'>
+           <div>
+             <img src="../../public/assets/images/luzon-18L.jpg" className="luzon-carousel"/>
+           </div>
+           <div>
+             <img src="../../public/assets/images/luzon-18L.jpg" className="luzon-carousel"/>
+           </div>
+           <div>
+             <img src="../../public/assets/images/luzon-18L.jpg" className="luzon-carousel"/>
+           </div>
+
+        </Slider>
+
       </div>
     )
   }
